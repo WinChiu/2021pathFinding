7dfc5ade789e26bc10e1f28792dcb2b5de2d8eaa
Win Chiu <win881206@gmail.com>
2021-03-29 14:12:10 +0800

    fix heroku v1.0

--- index.js
+++ index.js
@@ -6,7 +6,24 @@ const PORT = process.env.PORT || 8000;
 app.use("/", express.static(__dirname));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/test.html");
+  res.sendFile(__dirname + "/test.html");
 });
+
+app.use(function (req, res, next) {
+  if (req.headers["x-forwarded-proto"] === "https") {
+    res.redirect("http://" + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname));
+
+  // Hande SPA
+  app.use(/.*/, (req, res) => {
+    res.sendFile(__dirname + "/test.html");
+  }); // Refer to any route at all
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
