178d69f11b36800a06e86529f2c7902de652af21
Win Chiu <win881206@gmail.com>
2023-01-27 15:32:16 +0800

    deploy to render

--- public/styling/effect.js
+++ public/styling/effect.js
@@ -1,62 +1,61 @@
-import Board from "https://wsa-path-finding.herokuapp.com/public/browser/board.js";
-
-$("#startButtonDijkstra").click(function () {
-  $("#algorithmsOption").text($(this).text());
+import Board from '../browser/board';
+$('#startButtonDijkstra').click(function () {
+  $('#algorithmsOption').text($(this).text());
 });
-$("#startButtonGreedy").click(function () {
-  $("#algorithmsOption").text($(this).text());
+$('#startButtonGreedy').click(function () {
+  $('#algorithmsOption').text($(this).text());
 });
-$("#startButtonAStar2").click(function () {
-  $("#algorithmsOption").text($(this).text());
+$('#startButtonAStar2').click(function () {
+  $('#algorithmsOption').text($(this).text());
 });
-$("#startButtonAStar3").click(function () {
-  $("#algorithmsOption").text($(this).text());
+$('#startButtonAStar3').click(function () {
+  $('#algorithmsOption').text($(this).text());
 });
-$("#startButtonAStar").click(function () {
-  $("#algorithmsOption").text($(this).text());
+$('#startButtonAStar').click(function () {
+  $('#algorithmsOption').text($(this).text());
 });
-$("#startButtonBidirectional").click(function () {
-  $("#algorithmsOption").text($(this).text());
+$('#startButtonBidirectional').click(function () {
+  $('#algorithmsOption').text($(this).text());
 });
-$("#startButtonBFS").click(function () {
-  $("#algorithmsOption").text($(this).text());
+$('#startButtonBFS').click(function () {
+  $('#algorithmsOption').text($(this).text());
 });
-$("#startButtonDFS").click(function () {
-  $("#algorithmsOption").text($(this).text());
+$('#startButtonDFS').click(function () {
+  $('#algorithmsOption').text($(this).text());
 });
 
 // Adjust Speed
-$("#speedFast").click(function () {
-  $("#adjustspeed").text($(this).text());
+$('#speedFast').click(function () {
+  $('#adjustspeed').text($(this).text());
 });
-$("#speedAverage").click(function () {
-  $("#adjustspeed").text($(this).text());
+$('#speedAverage').click(function () {
+  $('#adjustspeed').text($(this).text());
 });
-$("#speedSlow").click(function () {
-  $("#adjustspeed").text($(this).text());
+$('#speedSlow').click(function () {
+  $('#adjustspeed').text($(this).text());
 });
 //Algorithms Options
-$("#startButtonDijkstrat").click(function () {
-  $("#algorithmsOptions").text($(this).text());
+$('#startButtonDijkstrat').click(function () {
+  $('#algorithmsOptions').text($(this).text());
 });
-$("#startButtonGreedy").click(function () {
-  $("#algorithmsOptions").text($(this).text());
+$('#startButtonGreedy').click(function () {
+  $('#algorithmsOptions').text($(this).text());
 });
-$("#startButtonAStar2").click(function () {
-  $("#algorithmsOptions").text($(this).text());
+$('#startButtonAStar2').click(function () {
+  $('#algorithmsOptions').text($(this).text());
 });
-$("#startButtonAStar3").click(function () {
-  $("#algorithmsOptions").text($(this).text());
+$('#startButtonAStar3').click(function () {
+  $('#algorithmsOptions').text($(this).text());
 });
-$("#startButtonAStar").click(function () {
-  $("#algorithmsOption").text($(this).text());
+$('#startButtonAStar').click(function () {
+  $('#algorithmsOption').text($(this).text());
 });
-$("#startButtonBidirectional").click(function () {
-  $("#algorithmsOption").text($(this).text());
+$('#startButtonBidirectional').click(function () {
+  $('#algorithmsOption').text($(this).text());
 });
-$("#startButtonBFS").click(function () {
-  $("#algorithmsOption").text($(this).text());
+$('#startButtonBFS').click(function () {
+  $('#algorithmsOption').text($(this).text());
 });
-$("#startButtonDFS").click(function () {
-  $("#algorithmsOption").text($(this).text());
+$('#startButtonDFS').click(function () {
+  $('#algorithmsOption').text($(this).text());
 });
